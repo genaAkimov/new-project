c4147ea83547b98c85fc3a156f1fcf584e490ad8
Akimov Gennady <akimov-gena@mail.ru>
2022-11-07 10:50:12 +0300

    added a variable: w.s

--- cod.py
+++ cod.py
@@ -2,7 +2,5 @@ print("Hello word!")
 a = 14
 b = 14
 print("Колличество яблок =", a + b)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 w = int(input("Продолжить покупки? 1/0"))
 s = int(input("Введите кол-во полученных яблок?"))

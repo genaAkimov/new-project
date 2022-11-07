3bf09b50ebda7837edab4fd82533d3169666c9e5
Akimov Gennady <akimov-gena@mail.ru>
2022-11-07 11:07:26 +0300

    a = 13, b = 13

--- cod.py
+++ cod.py
@@ -1,9 +1,5 @@
-<<<<<<< Updated upstream
-print("Hello word!")
-=======
 print("Hello word!")
 a = 12
 b = 12
 print("Колличество яблок =", a + b)
 s = int(input("Введите кол-во полученных яблок?"))
->>>>>>> Stashed changes

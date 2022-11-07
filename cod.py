c715351768b8e44fb55afdcfbe2f74359b61c12c
Akimov Gennady <akimov-gena@mail.ru>
2022-11-07 10:44:31 +0300

    added a variable: w,s

--- cod.py
+++ cod.py
@@ -2,8 +2,5 @@ print("Hello word!")
 a = 14
 b = 14
 print("Колличество яблок =", a + b)
-<<<<<<< Updated upstream
 w = int(input("Продолжить покупки? 1/0"))
-=======
->>>>>>> Stashed changes
 s = int(input("Введите кол-во полученных яблок?"))
